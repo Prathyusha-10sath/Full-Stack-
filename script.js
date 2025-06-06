01b66f3042a207bf25e992354e2a44cb0fbba12a
Prathyusha-10Sath <Prathyushaaa2006@gmail.com>
2025-06-06 14:54:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// function add(a,b){
+//       return (a+b)
+// }
+// console.log(add(20,30));
+// function showName(){
+//     const name=document.getElementById('Name').value 
+//     document.getElementById('result').innerText=`hello ${name}`}
+
+    const choice=['rock','paper','scissor']
+        let computer= choice[Math.floor(Math.random()*3)]
+        console.log(computer);
